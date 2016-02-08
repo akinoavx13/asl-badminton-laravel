1dc9a52d50f9c174f7e35a9d232566cd5dc253ce
Maxime Maheo <maxime.maheo@etu.u-bordeaux.fr>
2016-02-08 19:11:37 +0100

    no message

--- resources/views/rope/create.blade.php
+++ resources/views/rope/create.blade.php
@@ -56,7 +56,7 @@
             @if(count($ropes) > 0)
                 <div class="ibox float-e-margins">
                     <div class="ibox-content">
-                        <table class="table table-striped table-hover display" id="timeSlotsList">
+                        <table class="table table-striped table-hover display" id="ropesList">
                             <thead>
                             <tr>
                                 <th class="text-center">Nom</th>
@@ -98,5 +98,36 @@
             @endif
         </div>
     </div>
+@stop
 
+@section('javascript')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#ropesList').DataTable( {
+                "pageLength": 100,
+                language: {
+                    processing:     "Traitement en cours...",
+                    search:         "Rechercher&nbsp;:",
+                    lengthMenu:    "Afficher _MENU_ &eacute;l&eacute;ments",
+                    info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+                    infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+                    infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+                    infoPostFix:    "",
+                    loadingRecords: "Chargement en cours...",
+                    zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+                    emptyTable:     "Aucune donnée disponible dans le tableau",
+                    paginate: {
+                        first:      "Premier",
+                        previous:   "Pr&eacute;c&eacute;dent",
+                        next:       "Suivant",
+                        last:       "Dernier"
+                    },
+                    aria: {
+                        sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                        sortDescending: ": activer pour trier la colonne par ordre décroissant"
+                    }
+                }
+            } );
+        } );
+    </script>
 @stop
