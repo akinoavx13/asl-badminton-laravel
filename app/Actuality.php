82a6b707ec8f6d86453fba5f613f1ab96984ef1f
Maxime Maheo <maxime.maheo@etu.u-bordeaux.fr>
2016-02-22 13:19:59 +0100

    correction des photos

--- app/Actuality.php
+++ app/Actuality.php
@@ -56,7 +56,44 @@ class Actuality extends Model
     {
         if (is_object($photo) && $photo->isValid())
         {
-            ImageManagerStatic::make($photo)->fit(200)->save(public_path() . "/img/actualities/{$this->id}.jpg");
+
+            $image = ImageManagerStatic::make($photo);
+
+            if ($image->width() > 200 && $image->height() > 200)
+            {
+                $max = $image->width() > $image->height() ? 'width' : 'height';
+
+                if ($max == 'width')
+                {
+                    $image->resize(200, null, function ($constraint)
+                    {
+                        $constraint->aspectRatio();
+                    });
+                }
+                else
+                {
+                    $image->resize(null, 200, function ($constraint)
+                    {
+                        $constraint->aspectRatio();
+                    });
+                }
+            }
+            elseif($image->width() < 200 && $image->height() > 200)
+            {
+                $image->resize(null, 200, function ($constraint)
+                {
+                    $constraint->aspectRatio();
+                });
+            }
+            elseif($image->width() > 200 && $image->height() < 200)
+            {
+                $image->resize(200, null, function ($constraint)
+                {
+                    $constraint->aspectRatio();
+                });
+            }
+
+            $image->save(public_path() . "/img/actualities/{$this->id}.jpg");
             $this->attributes['photo'] = 1;
         }
     }
