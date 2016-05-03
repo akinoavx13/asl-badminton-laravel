ef004aa1a1e0607d0057921424747df8e539143f
Maxime Maheo <imaxame@gmail.com>
2016-05-03 18:46:11 +0200

    new design for tournament

--- resources/views/tournament/index.blade.php
+++ resources/views/tournament/index.blade.php
@@ -31,7 +31,7 @@
             <div class="panel-body">
 
                 <div class="table-responsive">
-                    <table class="table table-hover">
+                    <table class="table">
                         <tbody>
 
                         @for($nbMatchRank1 = 1; $nbMatchRank1 <= $serie['info']->number_matches_rank_1 * 2 - 1; $nbMatchRank1++)
@@ -40,62 +40,80 @@
                                     @if($serie[$rank][$nbMatchRank1] == "vide")
                                         <td style="border: none"></td>
                                     @else
+
                                         <td style="border: none">
-                                            <div class="panel panel-default" style="margin-bottom: 0;">
-                                                <div class="panel-body" style="padding: 10px;">
-                                                    <div class="text-center">
 
-                                                        <span>N°{{ $serie[$rank][$nbMatchRank1]['matchNumber'] }}</span>
+                                            <span style="font-weight: bold;">
+                                                N°{{ $serie[$rank][$nbMatchRank1]['matchNumber'] }}
+                                            </span>
 
-                                                        @if($serie[$rank][$nbMatchRank1]['score'] != null)
-                                                            @if($serie[$rank][$nbMatchRank1]['score']->hasFirstTeamWin(true))
-                                                                <span class="text-navy">{{ $serie[$rank][$nbMatchRank1]['firstTeamName'] }}</span>
-                                                                <span style="font-weight: bold;">&</span>
-                                                                <span>{{ $serie[$rank][$nbMatchRank1]['secondTeamName'] }}</span>
-                                                            @elseif($serie[$rank][$nbMatchRank1]['score']->hasSecondTeamWin(true))
-                                                                <span>{{ $serie[$rank][$nbMatchRank1]['firstTeamName'] }}</span>
-                                                                <span style="font-weight: bold;">&</span>
-                                                                <span class="text-navy">{{ $serie[$rank][$nbMatchRank1]['secondTeamName'] }}</span>
-                                                            @else
-                                                                <span>{{ $serie[$rank][$nbMatchRank1]['firstTeamName'] }}</span>
-                                                                <span style="font-weight: bold;">&</span>
-                                                                <span>{{ $serie[$rank][$nbMatchRank1]['secondTeamName'] }}</span>
-                                                            @endif
-                                                        @else
-                                                            <span>{{ $serie[$rank][$nbMatchRank1]['firstTeamName'] }}</span>
-                                                            <span style="font-weight: bold;">&</span>
-                                                            <span>{{ $serie[$rank][$nbMatchRank1]['secondTeamName'] }}</span>
-                                                        @endif
+                                            @if($serie[$rank][$nbMatchRank1]['edit'])
+                                                <span>
+                                                    <a href="{{ route('score.editTournament', [$serie[$rank][$nbMatchRank1]['scoreId'], str_replace(' ', '-', $serie[$rank][$nbMatchRank1]['firstTeamName']), str_replace(' ', '-', $serie[$rank][$nbMatchRank1]['secondTeamName'])]) }}">Editer</a>
+                                                </span>
+                                            @endif
 
-                                                        @if($serie[$rank][$nbMatchRank1]['edit'])
-                                                            <a href="{{ route('score.editTournament', [$serie[$rank][$nbMatchRank1]['scoreId'], str_replace(' ', '-', $serie[$rank][$nbMatchRank1]['firstTeamName']), str_replace(' ', '-', $serie[$rank][$nbMatchRank1]['secondTeamName'])]) }}">
-                                                                Editer
-                                                            </a>
-                                                        @endif
-                                                        @if($auth->hasRole('admin'))
-                                                            <a href="{{ route('match.edit', $serie[$rank][$nbMatchRank1]['id']) }}"
-                                                               class="text-danger">Admin</a>
-                                                        @endif
+                                            @if($auth->hasRole('admin'))
+                                                <span>
+                                                    <a href="{{ route('match.edit', $serie[$rank][$nbMatchRank1]['id']) }}" class="text-danger">Administrer</a>
+                                                </span>
+                                            @endif
+
+                                            <table class="table table-bordered" style="margin-bottom: 5px; {{ $serie[$rank][$nbMatchRank1]['score'] != null && $serie[$rank][$nbMatchRank1]['score']->hasFirstTeamWin(true) ? 'background: #DFF0D8;' : '' }}">
+                                                <tr class="text-center">
+                                                    <td>
+                                                        {{ $serie[$rank][$nbMatchRank1]['firstTeamName'] }}
+                                                    </td>
+                                                    <td>
                                                         @if($serie[$rank][$nbMatchRank1]['score'] != null && ($serie[$rank][$nbMatchRank1]['score']->hasFirstTeamWin(true) || $serie[$rank][$nbMatchRank1]['score']->hasSecondTeamWin(true)))
-                                                            <p class="text-center" style="margin-bottom: 0;">
-                                                                {{ $serie[$rank][$nbMatchRank1]['score']->first_set_first_team }}
-                                                                <span class="text-danger">/</span>
-                                                                {{ $serie[$rank][$nbMatchRank1]['score']->first_set_second_team }}
-                                                                -
-                                                                {{ $serie[$rank][$nbMatchRank1]['score']->second_set_first_team }}
-                                                                <span class="text-danger">/</span>
-                                                                {{ $serie[$rank][$nbMatchRank1]['score']->second_set_second_team }}
-                                                                @if($serie[$rank][$nbMatchRank1]['score']->third_set_first_team != 0 && $serie[$rank][$nbMatchRank1]['score']->third_set_second_team != 0)
-                                                                    -
-                                                                    {{ $serie[$rank][$nbMatchRank1]['score']->third_set_first_team }}
-                                                                    <span class="text-danger">/</span>
-                                                                    {{ $serie[$rank][$nbMatchRank1]['score']->third_set_second_team }}
-                                                                @endif
-                                                            </p>
+                                                            {{ $serie[$rank][$nbMatchRank1]['score']->first_set_first_team }}
+                                                        @else
+                                                            Ø
                                                         @endif
-                                                    </div>
-                                                </div>
-                                            </div>
+                                                    </td>
+                                                    <td>
+                                                        @if($serie[$rank][$nbMatchRank1]['score'] != null && ($serie[$rank][$nbMatchRank1]['score']->hasFirstTeamWin(true) || $serie[$rank][$nbMatchRank1]['score']->hasSecondTeamWin(true)))
+                                                            {{ $serie[$rank][$nbMatchRank1]['score']->second_set_first_team }}
+                                                        @else
+                                                            Ø
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                        @if($serie[$rank][$nbMatchRank1]['score'] != null && ($serie[$rank][$nbMatchRank1]['score']->hasFirstTeamWin(true) || $serie[$rank][$nbMatchRank1]['score']->hasSecondTeamWin(true)))
+                                                            {{ $serie[$rank][$nbMatchRank1]['score']->third_set_first_team }}
+                                                        @else
+                                                            Ø
+                                                        @endif
+                                                    </td>
+                                                </tr>
+                                            </table>
+
+                                            <table class="table table-bordered" style="{{ $serie[$rank][$nbMatchRank1]['score'] != null && $serie[$rank][$nbMatchRank1]['score']->hasSecondTeamWin(true) ? 'background: #DFF0D8;' : '' }}">
+                                                <tr class="text-center">
+                                                    <td>{{ $serie[$rank][$nbMatchRank1]['secondTeamName'] }}</td>
+                                                    <td>
+                                                        @if($serie[$rank][$nbMatchRank1]['score'] != null && ($serie[$rank][$nbMatchRank1]['score']->hasFirstTeamWin(true) || $serie[$rank][$nbMatchRank1]['score']->hasSecondTeamWin(true)))
+                                                            {{ $serie[$rank][$nbMatchRank1]['score']->first_set_second_team }}
+                                                        @else
+                                                            Ø
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                        @if($serie[$rank][$nbMatchRank1]['score'] != null && ($serie[$rank][$nbMatchRank1]['score']->hasFirstTeamWin(true) || $serie[$rank][$nbMatchRank1]['score']->hasSecondTeamWin(true)))
+                                                            {{ $serie[$rank][$nbMatchRank1]['score']->second_set_second_team }}
+                                                        @else
+                                                            Ø
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                        @if($serie[$rank][$nbMatchRank1]['score'] != null && ($serie[$rank][$nbMatchRank1]['score']->hasFirstTeamWin(true) || $serie[$rank][$nbMatchRank1]['score']->hasSecondTeamWin(true)))
+                                                            {{ $serie[$rank][$nbMatchRank1]['score']->third_set_second_team }}
+                                                        @else
+                                                            Ø
+                                                        @endif
+                                                    </td>
+                                                </tr>
+                                            </table>
                                         </td>
                                     @endif
                                 @endfor
