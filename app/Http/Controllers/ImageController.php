d327dd38a5a1840424dea9fda9ddae72b4887087
Maxime Maheo <imaxame@gmail.com>
2016-10-17 16:55:08 +0200

    return posts and actualities images

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -11,14 +11,26 @@ class ImageController extends Controller
 
     public static function routes($router) {
         $router->pattern('user_id', '[0-9]+');
+        $router->pattern('actuality_id', '[0-9]+');
+        $router->pattern('post_id', '[0-9]+');
 
-        $router->get('{user_id}', [
-            'uses' => 'ImageController@show',
-            'as'   => 'image.show',
+        $router->get('/users/{user_id}', [
+            'uses' => 'ImageController@userImage',
+            'as'   => 'image.userImage',
+        ]);
+
+        $router->get('/actualities/{actuality_id}', [
+            'uses' => 'ImageController@actualityImage',
+            'as'   => 'image.actualityImage',
+        ]);
+
+        $router->get('/posts/{post_id}', [
+            'uses' => 'ImageController@postImage',
+            'as'   => 'image.postImage',
         ]);
     }
 
-    public function show($user_id) {
+    public function userImage($user_id) {
 
         $path = public_path() . '/img/avatars/' . $user_id . '.jpg';
 
@@ -30,5 +42,31 @@ class ImageController extends Controller
 
         return Image::make(public_path() . '/img/anonymous.png')->response('png');
 
+    }
+
+    public function actualityImage($actuality_id) {
+
+        $path = public_path() . '/img/actualities/' . $actuality_id . '.jpg';
+
+        if (File::exists($path)) {
+            $img = Image::make($path)->response('jpg');
+
+            return $img;
+        }
+
+        return Image::make(public_path() . '/img/noImage.png')->response('png');
+    }
+
+    public function postImage($post_id) {
+
+        $path = public_path() . '/img/posts/' . $post_id . '.jpg';
+
+        if (File::exists($path)) {
+            $img = Image::make($path)->response('jpg');
+
+            return $img;
+        }
+
+        return Image::make(public_path() . '/img/noImage.png')->response('png');
     }
 }
