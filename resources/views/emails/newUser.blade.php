6f947b943f009dbdd4d6b96406e61b252296bb6d
Maxime Maheo <imaxame@gmail.com>
2016-11-10 15:25:17 +0100

    [UPD] Email of a new user

--- resources/views/emails/newUser.blade.php
+++ resources/views/emails/newUser.blade.php
@@ -5,13 +5,22 @@
 @stop
 
 @section('content')
-    <p>Bonjour {{ $forname }} {{ $name }},</p>
+    <p>Bonjour {{ $forname }},</p>
     <br>
 
     <p>
-        Nous venons de créer votre compte sur le site <a href="http://badminton.aslectra.com/">Badminton AS Lectra</a>.
+        Je viens de créer ton compte sur le site <a href="http://badminton.aslectra.com/">Badminton AS Lectra</a>.
     </p>
     <p>
-        Pour terminer la création de votre compte, merci de <a href="{{ route('user.get_first_connection', [$id, $token_first_connection]) }}">fournir quelques informations</a>.
+        Pour terminer la création de ton compte, merci de <a href="{{ route('user.get_first_connection', [$id, $token_first_connection]) }}">fournir quelques informations</a> en utilisant ce lien.
     </p>
+
+    <p>
+        Tu pourras ensuite via le site et le menu « s’inscrire à une saison » compléter ton profil et choisir ta formule de jeu.
+    </p>
+
+    <p>
+        Pour rappel le descriptif complet est sur le site de <a href="http://aslectra.com">l'aslectra</a>, je suis également dispo si tu as des questions. N’hésite pas à passer me voir.
+    </p>
+
 @stop
