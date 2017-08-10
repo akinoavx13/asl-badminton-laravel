220e48539f38c552225a3013108ddcf39ee4ec31
Maxime Maheo <imaxame@gmail.com>
2017-08-10 13:02:24 +0200

    Change mail for new subscriber

--- resources/views/emails/subscribeCompetitionFormula.blade.php
+++ resources/views/emails/subscribeCompetitionFormula.blade.php
@@ -13,9 +13,28 @@
     </p>
 
     <p>
-        Pour finaliser votre inscription, merci de bien vouloir remplir les papiers et de les remettre le plus tôt
-        possible à Christophe Mahéo.
+        Pour finaliser votre inscription, merci de bien vouloir remplir les formulaires ci-dessous et les transmettre le
+        plus tôt
+        possible à Christophe Mahéo
     </p>
+
+    <ul>
+        <li>
+            <p>
+                Si il s’agit de votre première demande de licence il faut remplir le <a href="{{ asset('documents/certificat_medical.pdf') }}">certificat médical</a>
+                et la
+                <a href="{{ asset('documents/formulaire_licence.pdf') }}">demande de licence</a>.
+            </p>
+        </li>
+        <li>
+            <p>
+                Si il s’agit d’un renouvellement de licence et que le certificat médical fournit l’année dernière était
+                daté à partir du 1er Juillet 2016, il faut remplir la <a href="{{ asset('documents/formulaire_licence.pdf') }}">demande de licence</a> et vous pouvez éviter le
+                certificat médical et ne fournir que le <a href="{{ asset('documents/questionnaire_sante.pdf') }}">questionnaire de santé</a> (sous réserve que vous répondrez NON à
+                toutes les questions, sinon il faudra tout de même un <a href="{{ asset('documents/certificat_medical.pdf') }}">certificat médical</a>)
+            </p>
+        </li>
+    </ul>
 
     <p>
         <a href="{{ asset('documents/badminton-Formulaire Certificat Medical 2015-2016.pdf') }}">Certificat médical</a>
